eec13a05a399df61cad8b6b65b09581b2622e219
Pranjal <pranjaldubey12345@gmail.com>
2025-03-15 23:51:06 +0530

    Created database schema for users, subjects, quizzes, and scores

---
+++ models.py
@@ -0,0 +1,59 @@
+from app import app
+from flask_sqlalchemy import SQLAlchemy
+from datetime import timedelta
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    passhash = db.Column(db.String(256), nullable=False)
+    name = db.Column(db.String(80), nullable=True)
+    is_admin = db.Column(db.Boolean, nullable=False, default=False)
+    qualifications = db.Column(db.String(256), nullable=True)
+    dob= db.Column(db.Date, nullable=True)
+
+    scores = db.relationship('Score', backref='user', lazy=True)
+
+class Subject(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(256), nullable=True)
+
+    chapters = db.relationship('Chapter', backref='subject', lazy=True, cascade='all, delete-orphan')
+
+class Chapter(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(256), nullable=True)
+    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id', ondelete='CASCADE'), nullable=False)
+
+    quizzes = db.relationship('Quiz', backref='chapter', lazy=True, cascade='all, delete-orphan')
+
+class Quiz(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_of_quiz = db.Column(db.Date, nullable=False)
+    time_duration = db.Column(db.Interval, nullable=False)
+    remarks = db.Column(db.String(256), nullable=True)
+    chapter_id = db.Column(db.Integer, db.ForeignKey('chapter.id', ondelete='CASCADE'), nullable=False)
+
+    questions = db.relationship('Question', backref='quiz', lazy=True, cascade='all, delete-orphan')
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id', ondelete='CASCADE'), nullable=False)
+    question_statement = db.Column(db.String(500), nullable=False)
+    option1 = db.Column(db.String(100), nullable=False)
+    option2 = db.Column(db.String(100), nullable=False)
+    option3 = db.Column(db.String(100), nullable=False)
+    option4 = db.Column(db.String(100), nullable=False)
+    correct_option = db.Column(db.String(100), nullable=False)
+
+
+
+class Score(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id', ondelete='CASCADE'), nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+    remarks = db.Column(db.String(256), nullable=True)
